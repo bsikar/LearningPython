b4834c1058a593302979afe9ce7aa677186e8911
EvanSlyter <82098815+EvanSlyter@users.noreply.github.com>
2022-09-14 15:08:59 -0500

    Add files via upload

---
+++ Math/lab1.py
@@ -0,0 +1,22 @@
+#1a
+from sympy import *
+import math
+print((79*((11.1**2)+(math.e**1.29)))/(2026-(5.1)**3))
+
+#1b
+print(math.cos((11*math.pi)/(12))*(1/(math.cos(math.radians(75))))+math.tan((7*math.pi)/(12)))
+
+#2
+x=2.01
+print(math.sqrt((x**2)-4)/(x-2))
+
+x=2.0001
+print(math.sqrt((x**2)-4)/(x-2))
+
+x=2.000001
+print(math.sqrt((x**2)-4)/(x-2))
+
+
+
+
+
